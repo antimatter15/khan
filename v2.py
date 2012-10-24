9d0a0b9f775b6471567d0908bdc2292dcb04af54
antimatter15 <antimatter15@gmail.com>
2012-10-23 22:24:16 -0400

    new readme and stuff

--- v2.py
+++ v2.py
@@ -4,9 +4,16 @@ mask_image = Image("mask.png").grayscale()
 cam = VirtualCamera("output.avi", 'video')
 last_frame = None
 img = None
+old_coords = (-1, -1)
+# old_masked = None
+last_complete = None
+
 
 def find_and_subtract_cursor(img):
 	found_patterns = img.findTemplate(pattern_image, 200)
+	if len(found_patterns) is not 1:
+		return (-1, -1, None)
+
 	x = found_patterns.x()[0]
 	y = found_patterns.y()[0]
 	# print found_patterns
@@ -15,23 +22,65 @@ def find_and_subtract_cursor(img):
 
 
 
-for i in range(50, 400):
+for i in range(10, 10473):
 	last_frame = img
-	img = cam.getFrame(i)
+	
+	next_img = cam.getFrame(i)
+	draw = DrawingLayer((next_img.width, next_img.height))
+	
+	x, y, new_mask = find_and_subtract_cursor(next_img)
+	
+	next_img = next_img.grayscale().binarize(60).invert()
+
+
+	if x < 0 or y < 0:
+		img = next_img
+		continue
+
+	# if old_masked is None:
+	# 	old_masked = Image((next_img.width, next_img.height))
+	
+	if last_complete is None:
+		last_complete = Image((next_img.width, next_img.height))
+
+	img = next_img - new_mask
+	
+	# old_mask = old_masked
+	# old_masked = new_mask
+
+	(old_x, old_y) = old_coords
+	old_coords = (x, y)
+
 	if last_frame is None:
 		continue
 
-	x, y, new_mask = find_and_subtract_cursor(img)
-	old_x, old_y, old_mask = find_and_subtract_cursor(last_frame)
+	draw.circle((x + 6, y + 1), 10, color=Color.RED)
 
-	sub = ((img.grayscale() - last_frame.grayscale()) - new_mask - old_mask).binarize(128)
+	
+	# old_x, old_y, old_mask = find_and_subtract_cursor(last_frame)
+
+	# if old_x < 0 or old_y < 0:
+	# 	continue
+	
+	sub = (img.copy() - last_complete).binarize(60)
+
+	last_complete = last_complete + img.copy()
+
+	
+	# sub = ((img.copy() - last_frame.copy()) - old_mask).binarize(60)
 	corners = sub.findCorners()
 	if corners is not None:
 		corners.draw()
 	# blobs = sub.findBlobs()
 	# if blobs is not None:
 	# blobs.draw(autocolor=True)
-	sub.show()
+	last_complete.addDrawingLayer(draw)
+	last_complete.addDrawingLayer(sub.getDrawingLayer())
+	last_complete.applyLayers()
+	last_complete.show()
+	# img.addDrawingLayer(draw)
+	# img.applyLayers()
+	# img.show()
 
 	# print found_patterns
 
@@ -39,7 +88,7 @@ for i in range(50, 400):
 	# print found_patterns
 	# print "--"
 
-
+	print i, (x, y), [(corner.x, corner.y) for corner in corners]
 	# (img.grayscale() - last_frame.grayscale()).show()
 
 
@@ -47,3 +96,4 @@ for i in range(50, 400):
 	# print i, found_patterns.x(), found_patterns.y()
 	# found_patterns.draw()
 	# img.show()
+
